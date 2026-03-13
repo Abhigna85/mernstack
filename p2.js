ece676808b46ab344725d35008bf9580e71b5a5c
Abhigna85 <abhignakanuma@gmail.com>
2026-03-13 11:52:42 +0530

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,15 @@
+let day=5;
+if(day==1){
+    console.log("monday");
+}
+else if(day==2){
+    console.log("tuesday");
+}
+else if(day==3){
+    console.log("wednesday");
+}
+else if(day==4){
+    console.log("thursday");
+}else{
+    console.log("no day");
+}
